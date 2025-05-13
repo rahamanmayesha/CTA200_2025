6b1be54f30d9e229354082b1d58140be7afd77dd
Mayesha Rahaman <mayesharahaman35@gmail.com>
2025-05-13 04:03:12 -0400

    Added assignment 3 material

---
+++ assignment_3/iter_func.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def iter_func(xmin, xmax, ymin, ymax, width, height, max_iter):
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
+    C = x[np.newaxis, :] + 1j * y[:, np.newaxis]  
+    Z = np.zeros_like(C, dtype=complex)
+    diverged = np.zeros(C.shape, dtype=int)
+    mask = np.full(C.shape, True, dtype=bool)
+
+    for i in range(max_iter):
+        Z[mask] = Z[mask] ** 2 + C[mask]
+        mask_new = np.abs(Z) <= 2
+        diverged[(mask) & (~mask_new)] = i
+        mask &= mask_new
+
+    diverged[diverged == 0] = max_iter  # treat those that never diverged
+    return diverged
